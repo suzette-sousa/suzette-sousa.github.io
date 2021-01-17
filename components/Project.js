e9271fdd6ad16f847a539464de18f535ba86e38e
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-01-17 15:27:23 +0100

    add functionality filterprojects by categories

--- components/Project.js
+++ components/Project.js
@@ -1,17 +1,34 @@
 const path = require('path');
 import { attributes } from '../content/portfolio.md';
+import React, { useState } from "react";
 import Link from './Link'
 const Project = () => {
-  let { title, projets, cat } = attributes;
+  let { title, projets } = attributes;
+
+  let catArr = []
+  const mapProjets = projets.map((p) => {
+    let cat = p.categories
+    cat.map((e) => {
+      catArr.push(e)
+    })
+  })
+
+  let uniqueCats = [...new Set(catArr)];
+  let categories = Array.from(uniqueCats).sort();
+
+  const [categoryName, setCategoryName] = useState("All");
 
   return (
     <>
-      <button className="bg-black text-white">Afficher tout</button>
-
+      <button className="bg-black text-white" onClick={() => setCategoryName("All")}>Afficher tout</button>
+      {categories.map((cat, j) => (
+          <div key={j}><button key={j} onClick={() => setCategoryName(cat)}>{cat}</button></div>
+        ))
+      }
       <article>
         <h1 className="mb-6 text-4xl uppercase tracking-wider font-extralight text-center">{title}</h1>
         <ul className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {projets.map((projet, k) => (
+          {projets.filter(projet => categoryName!== "All" ? projet.categories.includes(categoryName): projet).map((projet, k) => (
             <li key={k} className="overflow-hidden bg-white rounded-lg shadow-xl">
               <Link href={`${projet.link}`}>
                 <a>
@@ -20,11 +37,11 @@ const Project = () => {
                   {projet.description && <p className="p-4">{projet.description}</p>}
                 </a>
               </Link>
-              {projet.categories &&
-                projet.categories.map((e, j) => (
-                  <div key={j}><button key={j}>{e}</button></div>
+              {/* {projet.categories &&
+                projet.categories.map((cat, j) => (
+                  <div key={j}><button key={j}>{cat}</button></div>
                 ))
-              }
+              } */}
             </li>
           ))}
         </ul>
