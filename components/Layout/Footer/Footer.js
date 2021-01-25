ddd7688881c1708de8082357552acc56802f058b
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-01-25 17:42:44 +0100

    remove contactform from footer

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import ContactForm from '../../ContactForm'
 
 class Footer extends Component {
   render () {
     return (
       <footer className="flex justify-center items-center bg-gray-800 text-white text-center h-24">
-        <ContactForm />
         <p>Site en cours de développement - Teck Stack : Next.js, Tailwind, Netlify | Suzette Sousa - 2021</p>
       </footer>
     );
