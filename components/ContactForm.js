28b19ce1df2db5ae34f2c09ac2ff439de291e6c2
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-01-26 12:06:41 +0100

    test contactform

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,7 +1,7 @@
 const ContactForm = () => {
   return (
     <>
-      <form name="contact" data-netlify="true" data-netlify-honeypot="bot-field" data-netlify-recaptcha="true">
+      <form name="contactForm" method="POST" id="contact-form" data-netlify="true" data-netlify-honeypot="bot-field">
         <p class="hidden">
           <label id="contact-form-bot-label"
               >Don't fill this out if you're human: <input name="bot-field" aria-labelledby="contact-form-bot-label"
@@ -9,7 +9,7 @@ const ContactForm = () => {
         </p>
         <p><label>Votre e-mail * : <input required type="email" name="email" /></label></p>
         <p><label>Votre message: <textarea required name="message" rows="4"></textarea></label></p>
-        <div data-netlify-recaptcha="true" class="form-row"></div>
+        <input type="hidden" name="form-name" value="contactForm" />
         <p><button type="submit">Envoyer</button></p>
       </form>
     </>
