1591183b7847fa2896a3d132d35cabdf3c39911d
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-01-26 08:59:37 +0100

    test contactform

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -5,7 +5,6 @@ class Footer extends Component {
   render () {
     return (
       <footer className="flex justify-center items-center bg-gray-800 text-white text-center h-24">
-        <ContactForm />
         <p>Site en cours de développement - Tech Stack : Next.js, Tailwind, Netlify | Suzette Sousa - 2021</p>
       </footer>
     );
