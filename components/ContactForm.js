47f77e30f122b97de4a28f905e850aa095cb3583
Suzette Sousa <sousa.suzette.pro@gmail.com>
2021-01-26 11:47:15 +0100

    test contactform

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,11 +1,14 @@
 const ContactForm = () => {
   return (
     <>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+        <p class="hidden">
+          <label id="contact-form-bot-label"
+              >Don't fill this out if you're human: <input name="bot-field" aria-labelledby="contact-form-bot-label"
+          /></label>
+        </p>
         <p><label>Votre e-mail * : <input required type="email" name="email" /></label></p>
-
         <p><label>Votre message: <textarea required name="message" rows="4"></textarea></label></p>
-
         <p><button type="submit">Envoyer</button></p>
       </form>
     </>
--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -4,7 +4,7 @@ import ContactForm from '../../ContactForm'
 class Footer extends Component {
   render () {
     return (
-      <footer className="flex justify-center items-center bg-gray-800 text-white text-center h-24">
+      <footer className="bg-gray-800 text-white text-center min-h-24">
         <ContactForm />
         <p>Site en cours de développement - Tech Stack : Next.js, Tailwind, Netlify | Suzette Sousa - 2021</p>
       </footer>
